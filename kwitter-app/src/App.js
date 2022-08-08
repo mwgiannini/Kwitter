3354c3af5acb931dbff10e1f0c5623a9e0797f2b
AmorAcebo <78448458+AmorAcebo@users.noreply.github.com>
2022-08-08 19:25:29 -0400

    add router

--- kwitter-app/src/App.js
+++ kwitter-app/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MainPage from './pages/mainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,12 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Router>
+        <Route>
+          <Route path='/' element={<MainPage />} /> 
+        </Route>
+      </Router>
     </div>
   );
 }
