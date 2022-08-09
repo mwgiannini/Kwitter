2793389d38c9fb132c49a9e3f304dba6a9db9037
AmorAcebo <78448458+AmorAcebo@users.noreply.github.com>
2022-08-08 21:20:19 -0400

    added some component in homepage

--- kwitter-app/src/components/navbar.tsx
+++ kwitter-app/src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 // // Need to make this a class that will recieve props.
 // //we will store the right and middle elements in state
 // // then we will generate the bar based on what was given
--- kwitter-app/src/pages/mainPage.tsx
+++ kwitter-app/src/pages/mainPage.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import { Typography } from "@mui/material";
 
 
-function MainPage(){
+function MainPage() {
+    const [auth, setAuth] = React.useState(true);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setAuth(event.target.checked);
+    };
+
     return (
-        <div>
-            This is the homepage.
-        </div>
-    );  
+        <>
+            <FormGroup>
+                <FormControlLabel
+                    control={
+                        <Switch
+                            checked={auth}
+                            onChange={handleChange}
+                            aria-label="login switch"
+                        />
+                    }
+                    label={auth ? 'Logout' : 'Login'}
+                />
+            </FormGroup>
+            {auth ?
+                <Typography variant="h1" >logged in</Typography>
+                :
+                <Typography variant='h1'>Not logged in</Typography>
+            }
+        </>
+    );
 }
 export default MainPage;
