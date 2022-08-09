be0becc691052de210c9f61dabce102fbea80474
AmorAcebo <78448458+AmorAcebo@users.noreply.github.com>
2022-08-08 20:08:40 -0400

    added homepage

--- kwitter-app/src/App.tsx
+++ kwitter-app/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import MainPage from './pages/mainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +24,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ kwitter-app/src/pages/mainPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function MainPage(){
+    return (
+        <div>
+            This is the homepage.
+        </div>
+    );  
+    
+    }
+export default MainPage;
