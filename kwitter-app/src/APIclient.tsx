34626ea1449aebdd2627b09f5424e812490bf0c7
MichaelWilliam Giannini <michaelwilliam.giann@mymail.champlain.edu>
2022-08-09 22:06:52 -0400

    Write getTimeline APIClient

--- kwitter-app/src/APIclient.tsx
+++ kwitter-app/src/APIclient.tsx
@@ -1,7 +1,16 @@
 import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
-var APIclient  = {
+var APIclient = {
+    async getTimeline(username : string){
+        return await axios.post(`/api/getTimeline/${username}`, username) //this is where we send the request 
+            .then( (res) => {
+                return res
+            })
+            .catch((err) => {return err}
+        );  
+    },
+
     async login(data : Object){
         return await axios.post('/api/login', data) //this is where we send the request 
             .then( (res) => {
@@ -9,6 +18,6 @@ var APIclient  = {
             })
             .catch((err) => {return err}
         );  
-    },
+    }
 }
 
