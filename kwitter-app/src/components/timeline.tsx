8e28b3fc904c7b33173369e6773bbf4286fcf068
mwgiannini <89871491+mwgiannini@users.noreply.github.com>
2022-08-10 23:36:54 -0400

    Replace timeline component

--- kwitter-app/src/components/timeline.tsx
+++ kwitter-app/src/components/timeline.tsx
@@ -1,37 +1,76 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
-import Kweet from './kweet'
+import Fab from '@mui/material/Fab';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import AddIcon from '@mui/icons-material/Add';
+import MoreIcon from '@mui/icons-material/MoreVert';
+
 import APIClient from '../APIclient'
-import { Console } from 'console';
+
+
+const StyledFab = styled(Fab)({
+  position: 'absolute',
+  zIndex: 1,
+  top: -30,
+  left: 0,
+  right: 0,
+  margin: '0 auto',
+});
 
 export default function Timeline() {
-
   const [timeline, setTimeline] = React.useState<Array<Object>>([])
 
   React.useEffect(()=>{ 
-    APIClient.getTimeline('tut_the_gut').then(
+    APIClient.getTimeline('mw').then(
       (res) => {
         console.log(res)
         setTimeline(res.data.data[0])
       }
     )
   },[])
-
+  
   return (
-      <Paper sx={{
-        marginLeft: 'auto',
-        marginRight: 'auto'
-      }}
-      >
-      
-      {timeline.map((val:any, index:number)=>{
-        return(
-          <Kweet message={val.message} 
-                  username={val.username} 
-                  post_time={val.post_time}/>
-        )
-      })}
-
+    <React.Fragment>
+      <CssBaseline />
+      <Paper square sx={{ pb: '50px' }}>
+        <Typography variant="h5" gutterBottom component="div" sx={{ p: 2, pb: 0 }}>
+          Inbox
+        </Typography>
+        <List sx={{ mb: 2 }}>
+          {timeline.map((val:any, index:number) => (
+            <React.Fragment>
+              <ListItem button>
+                <ListItemAvatar>
+                  <Avatar alt="Profile Picture" />
+                </ListItemAvatar>
+                <ListItemText primary={val.username} secondary={val.message} />
+              </ListItem>
+            </React.Fragment>
+          ))}
+        </List>
       </Paper>
+      <AppBar position="fixed" color="primary" sx={{ top: 'auto', bottom: 0 }}>
+        <Toolbar>
+          <StyledFab color="secondary" aria-label="add">
+            <AddIcon />
+          </StyledFab>
+          <Box sx={{ flexGrow: 1 }} />
+          <IconButton color="inherit">
+            <MoreIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
   );
 }
